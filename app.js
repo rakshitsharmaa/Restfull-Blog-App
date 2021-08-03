4bd2c58d86cfa85e6e40ad7c28d2691edc5c0349
rakshit <rakshitsharma268@gmail.com>
2021-08-03 18:55:12 +0530

    final issue resolved and blog app is online

--- app.js
+++ app.js
@@ -90,6 +90,6 @@ app.delete('/blogs/:id', (req, res) => {
         res.redirect("/blogs");
     })
 });
-app.listen(3000,function(){
+app.listen(process.env.PORT,function(){
     console.log("server is running");
 })
